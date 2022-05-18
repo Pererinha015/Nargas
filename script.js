aa6b50c4656991b42d5e8268a7529e3055e7f674
Perera <daniboydiefenthaeler@gmail.com>
2022-05-18 19:21:33 -0300

    adicionando buttom talvez

--- script.js
+++ script.js
@@ -2,10 +2,28 @@ var nargas = false
 
 function saia() {
     if (nargas == false) {
-        nargas = true
+        console.log('abrindo nao-nargas');
+        nargas = true;
         document.getElementById('nao-nargas').style.display = 'flex';
+        document.getElementById('talvez-nargas').style.display = 'none';
     } else {
-        nargas = false
+        console.log('fechando nao-nargas');
+        nargas = false;
         document.getElementById('nao-nargas').style.display = 'none';
+    }
+}
+
+var tal = false 
+
+function talvez() {
+    if (tal == false) {
+        console.log('abrindo talvez-nargas');
+        tal = true;
+        document.getElementById('talvez-nargas').style.display = 'flex';
+        document.getElementById('nao-nargas').style.display = 'none';
+    } else {
+        console.log('fechando talvez-nargas');
+        tal = false;
+        document.getElementById('talvez-nargas').style.display = 'none';
     }
 }
